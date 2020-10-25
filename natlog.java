eec88ca1cdefb9e8a5504420ec864d9c70b0a146
Anders Sjöberg <anders.sjoberg@calabrio.com>
2020-10-25 22:58:26 +0100

    Added comments to the java example

--- natlog.java
+++ natlog.java
@@ -1,12 +1,14 @@
 public class natlog {
     public static void main(String [] args) {
+        // set the start values
         double eNew = 1, eOld = 0;
         int factorial = 1, n = 1;
+        // do the things in { } until the statement in the while(...) is true
         do {
-            factorial *= n++; 
-            eOld = eNew;
-            eNew = eOld + 1D / factorial;
+            factorial *= n++; //multiply factorial with n then increase n by 1
+            eOld = eNew; // store eNew value in eOld to use for comparison in the while statement below
+            eNew = eOld + 1D / factorial; // calculate new value for eNew
             System.out.println(eNew);
-        } while (Math.abs(eNew - eOld) > 1E-5);
+        } while (Math.abs(eNew - eOld) > 1E-5); //check if the difference between the old and the new value is bigger than 0.00001. If it is do the loop again
     }
 }
