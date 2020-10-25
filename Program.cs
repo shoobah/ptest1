1c6fbccab8ef2216963a28bc81079fbb7f4495a6
Anders Sjöberg <anders.sjoberg@calabrio.com>
2020-10-25 22:48:38 +0100

    Added C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ptest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double eNew = 1, eOld = 0;
+            int factorial = 1, n = 1;
+            do
+            {
+                factorial *= n++;
+                eOld = eNew;
+                eNew = eOld + 1D / factorial;
+                Console.WriteLine(eNew);
+            } while (Math.Abs(eNew - eOld) > 1E-5);
+        }
+    }
+}
